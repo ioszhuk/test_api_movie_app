82a29f372448fa368fb65d900e14609fce500d88
ioszhuk <ioszhuk@gmail.com>
2023-05-02 23:47:09 +0200

    fix

--- src/routers/mainRouter.ts
+++ src/routers/mainRouter.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-
+import {Request, Response} from 'express';
 import {movieRouter} from './movieRouter';
 import {genreRouter} from './genreRouter';
 
@@ -8,3 +8,7 @@ export const mainRouter: Router = express.Router();
 mainRouter.use('/movies', movieRouter);
 
 mainRouter.use('/genres', genreRouter);
+
+mainRouter.use('/', async (req: Request, res: Response) => {
+  res.send('Application is running!');
+});
